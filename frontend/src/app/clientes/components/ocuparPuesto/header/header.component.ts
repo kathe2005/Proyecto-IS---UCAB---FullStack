cfbb377181add705a78807030dbbd4f4a8000af7
Cristian795-cpu <cateran.23@est.ucab.edu.ve>
2025-11-11 18:55:30 -0400

    header componentes

---
+++ frontend/src/app/clientes/components/ocuparPuesto/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  fechaActual: string = '';
+
+  ngOnInit() {
+    this.actualizarFecha();
+  }
+
+  actualizarFecha() {
+    const opciones: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    this.fechaActual = new Date().toLocaleDateString('es-ES', opciones);
+  }
+}
