745625250bddabc0de99815fd0d7d75969439094
Cristian795-cpu <cateran.23@est.ucab.edu.ve>
2025-11-12 06:21:18 -0400

    App component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router'; 
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    
-    provideHttpClient() 
+    provideRouter(routes),
+
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
